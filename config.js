a5f1a11fda31196e03bcbc681b5873ab33a24bf1
cliquetintas-art <cliquetintas@gmail.com>
2026-02-21 20:54:37 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const empresa = {
+  nome: "Tintas Qualy Quimy",
+  cidade: "Itaquaquecetuba",
+  telefone: "5511954950044",
+  endereco: "Rua Leiria, 45 - SP",
+  descricao: "Fábrica de tintas com alto rendimento e entrega rápida.",
+  instagram: "https://instagram.com/tintasqualyquimy"
+};
